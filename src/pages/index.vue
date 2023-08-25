1441b18086b67d507b58451b55dab53f7e2f6b58
SoulOriginal <mishaleonn@gmail.com>
2023-08-25 15:16:27 +0200

    index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -163,12 +163,12 @@
             <p class="font-400 xxxl:w-1000px lt-sm:w-full lt-sm:text-16px lt-xxxl:w-800px lt-xxl:w-576px xxxl:text-34px lt-xxxl:text-28px lt-xxl:text-20px lt-xl:text-20px leading-130% text-#1E2947/80 mt-14px lt-xxxl:mt-32px">
                 Русский Язык — код, в котором зашифрована мудрость веков истории России. Давай проверим,<br> насколько хорошо ты знаешь этот код!
             </p>
-            <div class="flex lt-sm:flex-col items-center justify-center mt-72px gap-x-22px gap-y-14px">
+            <div class="flex lt-sm:flex-col items-center justify-center lt-xxl:mt-34px mt-72px gap-x-22px gap-y-14px">
                 <button class="p-22px bg-#fff w-322px lt-xxxl:w-260px lt-sm:w-176px lt-sm:p-10px lt-xxxl:p-16px q-shadow-sm__light-green border-1px border-#C9C9C9" @click="easyMode = true; showGagarin = true;">
-                    <p class="text-20px l t-xxxl:text-18px lt-sm:text-14px leading-26px text-black">Легкий уровень</p>
+                    <p class="text-20px l t-xxxl:text-18px lt-sm:text-14px leading-26px text-black">Изи уровень</p>
                 </button>
                 <button class="p-22px bg-#fff w-322px lt-xxxl:w-260px lt-sm:w-176px lt-sm:p-10px lt-xxxl:p-16px q-shadow-sm border-1px border-#C9C9C9" @click="easyMode = false; showGagarin = true; ">
-                    <p class="text-20px l t-xxxl:text-18px lt-sm:text-14px leading-26px text-black">Сложный уровень</p>
+                    <p class="text-20px l t-xxxl:text-18px lt-sm:text-14px leading-26px text-black">Хард уровень</p>
                 </button>
             </div>
         </div>
@@ -177,16 +177,16 @@
 			<div v-if="showGagarin && !showGuideModal && !gameStarted">
 				<div class="fixed sm:hidden lt-sm:flex bottom-0 flex w-full left-0 right-0">
 					<img class="xxxl:w-550px lt-sm:w-210px lt-xxxl:w-400px lt-xxl:w-380px lt-xl:w-380px lt-lg:w-478px" style="transform: scaleX(-1);" src="/img/characters/grozny-nobg.png">
-					<div class="absolute bottom-180px lt-md:bottom-80px right-100px flex lt-sm:flex-col min-w-270px items-center h-100px justify-between gap-y-7px py-10px px-20px relative border-2px border-#C9C9C9 bg-white">
+					<div  class="absolute bottom-180px lt-md:bottom-80px right-100px flex lt-sm:flex-col items-center min-w-238px h-94px justify-between gap-y-7px py-10px px-20px relative border-2px border-#C9C9C9 bg-white">
 						<p class="font-500 xxxl:text-26px xxl:text-22px lt-xxl:text-16px lt-xl:text-16px leading-130%px">А как по-русски?</p>
-						<button @click="showGuideModal = true; showGagarin = false" class="py-12px lt-md:py-4px lt-sm:h-40px xxxl:text-20px lt-xxl:text-14px lt-xl:text-14px bg-#71CB8A w-260px lt-lg:w-240px leading-140% q-shadow-sm__lightest-green text-white">{{ easyMode ? 'Не изи уровень, а легкий' : 'Не хард уровень, а сложный' }}</button>
+						<button  :class="easyMode ? ['w-167px'] : ['w-176px']"  @click="showGuideModal = true; showGagarin = false" class="py-12px lt-md:py-4px lt-sm:h-40px xxxl:text-20px lt-xxl:text-14px lt-xl:text-14px bg-#71CB8A leading-140% q-shadow-sm__lightest-green text-white">{{ easyMode ? 'Не изи уровень, а легкий' : 'Не хард уровень, а сложный' }}</button>
 					</div>
 				</div>
 				<div class="fixed left-40% lt-sm:hidden lt-sm:left-unset lt-sm:right-30px lt-sm:translate-x-unset lt-xxl:left-33% translate-x-[-50%] bottom-0 flex">
 					<img class="xxxl:w-550px lt-sm:w-210px lt-xxxl:w-400px lt-xxl:w-380px lt-xl:w-380px lt-lg:w-478px" style="transform: scaleX(-1);" src="/img/characters/grozny-nobg.png">
 					<div class="flex lt-sm:flex-col items-center justify-between gap-x-26px py-20px lt-lg:py-14px px-34px relative border-2px border-#C9C9C9 h-100px lt-lg:h-76px bg-white -ml-100px lt-sm:m-0! xxxl:min-w-570px lt-xxxl:min-w-520px lt-xxl:min-w-380px lt-xl:min-w-380px lt-lg:absolute lt-lg:bottom-330px lt-lg:ml-50% lt-lg:w-400px lt-lg:gap-x-0 lt-sm:max-w-230px! lt-sm:w-230px! lt-sm:min-w-230px!">
 						<p class="font-500 xxxl:text-26px xxl:text-22px lt-xxl:text-16px lt-xl:text-16px leading-130%px">А как по-русски?</p>
-						<button @click="showGuideModal = true; showGagarin = false" class="py-12px xxxl:text-20px lt-xxl:text-14px lt-xl:text-14px bg-#71CB8A w-260px lt-xxl:w-153px lt-lg:w-153px leading-140% q-shadow-sm__lightest-green text-white">{{ easyMode ? 'Не изи уровень, а легкий' : 'Не хард уровень, а сложный' }}</button>
+						<button @click="showGuideModal = true; showGagarin = false" :class="easyMode ? ['w-320px'] : ['w-344px']" class="h-52px py-12px xxxl:text-18px lt-xxl:text-14px lt-xl:text-14px bg-#71CB8A w-260px lt-xxl:w-153px lt-lg:w-153px leading-140% q-shadow-sm__lightest-green text-white">{{ easyMode ? 'Не изи уровень, а легкий' : 'Не хард уровень, а сложный' }}</button>
 					</div>
 				</div>
 			</div>
